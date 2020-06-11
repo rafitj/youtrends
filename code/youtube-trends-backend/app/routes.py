98d2b18eada7c8d5d25de1501e63ffe32b991822
rafitj <rafit.jamil@gmail.com>
2020-06-11 19:57:49 -0400

    add insert

--- code/youtube-trends-backend/app/routes.py
+++ code/youtube-trends-backend/app/routes.py
@@ -1,11 +1,24 @@
 import logging
 
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 
-from app import app, models
+from app import app, models, db
 
 
 @app.route("/videos", methods=['GET'])
 def getAllVideos():
     videos = models.Video.query.all()
     return jsonify([video.serialize() for video in videos])
+
+
+
+@app.route("/video", methods=['POST'])
+def insertVideo():
+    title = request.form.get('tite')
+    id = request.form.get('id')
+    video = models.Video(id=id, title=title)
+    db.session.add(video)
+    db.session.commit()
+    return jsonify(video.serialize())
+
+

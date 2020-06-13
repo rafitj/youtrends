d11ccad162930db9fcb6ddef7046c99eddc217b6
rafitj <rafit.jamil@gmail.com>
2020-06-13 01:46:30 -0400

    update serializers and add sample data

--- code/youtube-trends-backend/app/models.py
+++ code/youtube-trends-backend/app/models.py
@@ -1,25 +1,32 @@
 import json
 
 from app import db
-from sqlalchemy import String, Column, Integer, ForeignKey
+from sqlalchemy import String, Column, Integer, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
+
 class Video(db.Model):
-    id = Column(Integer, primary_key=True)
-    title = Column(String(80), nullable=False)
-    # category_id = Column(Integer)
-    # channel = Column(Integer)
-    # publish_time = Column(String(80))
-    # tags = Column(String(80))
-    # likes= Column(Integer)
-    # views = Column(Integer)
-    # dislikes = Column(Integer)
-    # channel_title = Column(String(80))
+    id = Column(String(255), primary_key=True)
+    title = Column(String(255), nullable=False)
+    category_id = Column(Integer)
+    channel_id = Column(String(255), nullable=False)
+    publish_time = Column(Date, nullable=False)
+    tags = Column(String(255))
+    likes = Column(Integer, nullable=False)
+    views = Column(Integer, nullable=False)
+    dislikes = Column(Integer, nullable=False)
 
     def serialize(self):
-         return {
+        return {
             'id': self.id,
             'title': self.title,
+            'category_id': self.category_id,
+            'channel_id': self.channel_id,
+            'publish_time': self.publish_time,
+            'tags': self.tags,
+            'views': self.views,
+            'likes': self.likes,
+            'dislikes': self.dislikes,
         }
 
     def __repr__(self):
@@ -27,9 +34,9 @@ class Video(db.Model):
 
 
 class User(db.Model):
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(80), nullable=False)
-    last_name = Column(String(80))
+    id = Column(String(255), primary_key=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
 
     def serialize(self):
         return {
@@ -41,13 +48,13 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.id
 
-class Playlist(db.Model):
-    id = Column(Integer, primary_key=True)
-    title = Column(String(80), nullable=False)
-    user_id = Column(Integer, ForeignKey(User.id))
-    
-    user = relationship('User', foreign_keys='Playlist.user_id')
 
+class Playlist(db.Model):
+    id = Column(String(255), primary_key=True)
+    title = Column(String(255), nullable=False)
+    user_id = Column(String(255), ForeignKey(User.id))
+
+    user = relationship('User', foreign_keys='Playlist.user_id')
 
     def serialize(self):
         return {
@@ -61,15 +68,17 @@ class Playlist(db.Model):
 
 
 class PlaylistVideo(db.Model):
-    id = Column(Integer, primary_key=True)
-    playlist_id = Column(Integer, ForeignKey(Playlist.id))
-    video_id = Column(Integer, ForeignKey(Video.id))
-    
+    id = Column(String(255), primary_key=True)
+    playlist_id = Column(String(255), ForeignKey(Playlist.id))
+    video_id = Column(String(255), ForeignKey(Video.id))
+
     video = relationship('Video', foreign_keys='PlaylistVideo.video_id')
-    playlist = relationship('Playlist', foreign_keys='PlaylistVideo.playlist_id')
+    playlist = relationship(
+        'Playlist', foreign_keys='PlaylistVideo.playlist_id')
 
     def serialize(self):
         return {
+            'id': self.id,
             'playlist_id': self.playlist_id,
             'video_id': self.video_id,
         }

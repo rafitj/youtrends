2bef2ae0a8b45542d6817957cb30466cddb131b5
shrabdi <shrabdi@uwaterloo.ca>
2020-07-17 01:31:28 -0400

    added indexing and proper descriptions

--- milestone_2/test-production.sql
+++ milestone_2/test-production.sql
@@ -1,22 +1,24 @@
 -- We used Flask SQLAlchemy as our application ORM 
 -- We have translated our code into raw SQL syntax
 
--- DESCRIPTION: get all videos
+-- DESCRIPTION: get top 10 videos 
 -- SQL ALCHEMY: videos = models.Video.query.all()
 SELECT * FROM video LIMIT 10;
 
--- DESCRIPTION: get all videos ordered by views in descending order
+-- DESCRIPTION: get top 10 videos ordered by views in descending order
 -- SQL ALCHEMY: videos = models.Video.query.order_by(models.Video.views.desc()).all()
 SELECT * FROM video ORDER BY video.views DESC LIMIT 10;
 
--- DESCRIPTION: get all playlists 
+-- DESCRIPTION: get top 10 playlists 
 -- SQL ALCHEMY: playlists = models.Playlist.query.all()
 SELECT * FROM playlist LIMIT 10;
 
+
+-- DESCRIPTION: get all playlist videos
 SELECT * FROM playlist_video;
 -- No need to limit since there is only 1 tuple
 
--- DESCRIPTION: get the videos of a playlist given the playlist id
+-- DESCRIPTION: get all videos that are also in a playlist
 -- SQL ALCHEMY: 
 --     videos = models.Video.query\
 --     .join(PlaylistVideo, models.Video.id == models.PlaylistVideo.video_id)
@@ -24,3 +26,8 @@ SELECT * FROM playlist_video;
 SELECT video.* FROM video, playlist_video 
 WHERE video.id=playlist_video.video_id;
 -- No need to limit since only 1 is returned in our example
+
+-- DESCRIPTION: Index trending_country and publish_time attributes for more efficiency
+CREATE INDEX country_index ON video (trending_country);
+
+CREATE INDEX publish_time_index ON video (publish_time);
